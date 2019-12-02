02e38d3c20e2b6c5eeaa1c08a4a6821438698c0c
AmoDinho <u14284783@tuks.co.za>
2019-12-02 14:42:08 +0200

    Created a body text

---
+++ src/components/typography/BodyText.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BodyText = ({ children }) => {
+    return (
+        <>
+            <p className="font-sans text-base">{children}</p>
+        </>
+    )
+}
+
+export default BodyText
