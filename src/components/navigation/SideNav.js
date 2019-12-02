272ce18e423e214e26f3ca4eff9489de4cda8105
AmoDinho <u14284783@tuks.co.za>
2019-12-02 14:26:58 +0200

    created sidenav component

---
+++ src/components/navigation/SideNav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideNav = () => {
+    return (
+        <>
+            <div>
+                SIDE NAV
+</div>
+        </>
+    )
+}
+
+export default SideNav 
