7ed8073a583fda0903cc47df955e9b8edf0e82d8
AmoDinho <u14284783@tuks.co.za>
2019-12-02 16:50:59 +0200

    created a dropdown component

---
+++ src/components/dropdowns/ClientDropdown.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ClientDropdown = () => {
+    return (
+        <>
+            <select>
+
+            </select>
+
+        </>
+    )
+}
+
+export default ClientDropdown
