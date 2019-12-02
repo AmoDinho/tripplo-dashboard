3843c1881d953f3871175041a7eb9e4cf7b98cae
AmoDinho <u14284783@tuks.co.za>
2019-12-02 16:44:55 +0200

    all the icons are in

--- src/components/navigation/SideNav.js
+++ src/components/navigation/SideNav.js
@@ -2,6 +2,11 @@ import React from 'react'
 import TripploLogo from '../../assets/tripplo-logo-white.png'
 import BodyText from '../typography/BodyText'
 import OrgIcon from '../../assets/view_compact.svg'
+import ProfitIcon from '../../assets/signal_cellular_alt.svg'
+import DateIcon from '../../assets/date_range.svg'
+import TruckIcon from '../../assets/local_shipping.svg'
+import CardIcon from '../../assets/credit_card.svg'
+import TrackingIcon from '../../assets/pin_drop.svg'
 const SideNav = () => {
     return (
         <>
@@ -22,20 +27,20 @@ const SideNav = () => {
                         </li>
                         <li >
                             <div className="flex flex-row mt-3 border-l-4 block p-2 ">
-                                <img src={OrgIcon} alt="org icon" className="mr-2 " />    <BodyText className="text-white ">Fleet</BodyText>
+                                <img src={TruckIcon} alt="org icon" className="mr-2 " />    <BodyText className="text-white ">Fleet</BodyText>
                             </div>
                         </li>
                         <li className="flex flex-row mt-3 p-2">
-                            <img src={OrgIcon} alt="org icon" className="mr-2" />    <BodyText className="text-white">Trip planning</BodyText>
+                            <img src={DateIcon} alt="org icon" className="mr-2" />    <BodyText className="text-white">Trip planning</BodyText>
                         </li>
                         <li className="flex flex-row mt-3 p-2">
-                            <img src={OrgIcon} alt="org icon" className="mr-2" />    <BodyText className="text-white">Trip tracking</BodyText>
+                            <img src={TrackingIcon} alt="org icon" className="mr-2" />    <BodyText className="text-white">Trip tracking</BodyText>
                         </li>
                         <li className="flex flex-row mt-3 p-2">
-                            <img src={OrgIcon} alt="org icon" className="mr-2" />    <BodyText className="text-white">Profit analysis</BodyText>
+                            <img src={ProfitIcon} alt="org icon" className="mr-2" />    <BodyText className="text-white">Profit analysis</BodyText>
                         </li>
                         <li className="flex flex-row mt-3 p-2">
-                            <img src={OrgIcon} alt="org icon" className="mr-2" />    <BodyText className="text-white">Biling</BodyText>
+                            <img src={CardIcon} alt="org icon" className="mr-2" />    <BodyText className="text-white">Biling</BodyText>
                         </li>
                     </ul>
                 </div>
