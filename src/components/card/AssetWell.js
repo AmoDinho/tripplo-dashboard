da50faec6b885f2f719f085e45de0924938b0a7c
AmoDinho <u14284783@tuks.co.za>
2019-12-03 16:12:44 +0200

    asset well is fixed

--- src/components/card/AssetWell.js
+++ src/components/card/AssetWell.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import BodyText from '../typography/BodyText'
 const AssetWell = () => {
     return (
-        <div className="rounded bg-white border border-gray-600">
-
-        </div>
+        <div className='rounded bg-white border border-gray-400 flex flex-col w-1/4 p-3 ' >
+            <div className='flex justify-between '>
+                <BodyText>Needs review</BodyText>
+                <BodyText className='w-4 h-5 text-white text-center bg-yellow-500 mr-2'>3</BodyText>
+            </div>
+            <div className='flex justify-between '>
+                <BodyText>Urgent attention</BodyText>
+                <BodyText className='w-4 h-5 text-white text-center bg-red-500 mr-2'>1</BodyText>
+            </div>
+        </div >
     )
 }
 
--- src/components/fleet/AssetRow.js
+++ src/components/fleet/AssetRow.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AssetCard from '../card/AssetCard'
+import AssetWell from '../card/AssetWell'
 const AssetRow = () => {
     return (
         <>
             <div>
                 <AssetCard assetName='Truck' assetColour='500' assetCount='4' />
+                <AssetWell />
             </div>
         </>
     )
