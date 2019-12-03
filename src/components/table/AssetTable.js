b1ddc2a05c4a775fe946ef0bb9e437604400361d
AmoDinho <u14284783@tuks.co.za>
2019-12-03 17:57:36 +0200

    Ready to go

--- src/components/table/AssetTable.js
+++ src/components/table/AssetTable.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table'
 import StatusDropdown from '../dropdowns/StatusDropdown'
+import BodyText from '../typography/BodyText'
 import 'bootstrap/dist/css/bootstrap.min.css'
 const AssetTable = () => {
     return (
         <>
             <div className='p-4'>
                 <div className='flex flex-row'>
-                    <div className='bg-blue-700 w-1/12 h-16 rounded-t text-center p-3'>Truks</div>
-                    <diV className='bg-blue-500 w-full h-16 rounded-t p-3 p-3 '>Issues</diV>
+                    <div className='bg-blue-700 w-1/12 h-16 rounded-t text-center p-3'> <BodyText className="text-white">Truks</BodyText></div>
+                    <diV className='bg-blue-200 w-full h-16 rounded-t p-3 p-3 '> <BodyText className="text-gray-600 ">Issues</BodyText></diV>
                 </div>
                 <Table>
                     <thead>
