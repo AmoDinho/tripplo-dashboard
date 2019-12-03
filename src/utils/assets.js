c94deb9530d2d9e177b35221cdb943a37afd977b
AmoDinho <u14284783@tuks.co.za>
2019-12-03 16:24:41 +0200

    created assets.js

---
+++ src/utils/assets.js
@@ -0,0 +1,17 @@
+export const assets = [
+    {
+        assetName: 'Trucks',
+        assetColour: '400',
+        assetCount: '4'
+    },
+    {
+        assetName: 'Trailers',
+        assetColour: '500',
+        assetCount: '8'
+    },
+    {
+        assetName: 'Drivers',
+        assetColour: '600',
+        assetCount: '4'
+    }
+]
