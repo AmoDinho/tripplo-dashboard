b35d7e04fdc37d3f3bff8445a0a1a72a113a5e25
AmoDinho <u14284783@tuks.co.za>
2019-12-03 15:32:17 +0200

    added line break

--- src/components/fleet/AddAsset.js
+++ src/components/fleet/AddAsset.js
@@ -4,9 +4,12 @@ import HeadingOne from '../typography/HeadingOne'
 const AddAsset = () => {
     return (
         <>
-            <div className="flex flex-row">
-                <HeadingOne>Fleet Management</HeadingOne>
-                <BlueButton>Add Asset</BlueButton>
+            <div className="flex flex-col">
+                <div className="flex flex-ro ml-20">
+                    <HeadingOne>Fleet Management</HeadingOne>
+                    <BlueButton>Add Asset</BlueButton>
+                </div>
+                <hr className="w-4/5 ml-20" />
             </div>
 
         </>
