f5efcd9802f7ad609c658b7b59edb7a29b2bd540
AmoDinho <u14284783@tuks.co.za>
2019-12-03 16:03:24 +0200

    created asset well

---
+++ src/components/card/AssetWell.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AssetWell = () => {
+    return (
+        <div className="rounded bg-white border border-gray-600">
+
+        </div>
+    )
+}
+
+export default AssetWell
