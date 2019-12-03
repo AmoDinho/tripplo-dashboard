34b6c8a27c5482a6fda3b8adc9f4ac9f14f4976f
AmoDinho <u14284783@tuks.co.za>
2019-12-03 11:46:32 +0200

    added the bell and grid

--- src/components/navigation/TopNav.js
+++ src/components/navigation/TopNav.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Menu from '../../assets/menu.svg'
 import SearchBar from '../inputs/SearchBar'
+import Bell from '../../assets/notification.svg'
+import Grid from '../../assets/grid.svg'
 const TopNav = ({ isOpen, onClick, children, ...props }) => {
     return (
         <nav className={isOpen ? 'h-12 p-10 bg-gray-300 mb-5 flex flex-row' : 'h-12 p-10 bg-gray-300 mb-5 w-screen flex flex-row'}>
@@ -10,7 +12,11 @@ const TopNav = ({ isOpen, onClick, children, ...props }) => {
             </span>
 
             <SearchBar />
+            <div>
+                <img src={Grid} alt="grid" />
+                <img src={Bell} alt="bell" />
 
+            </div>
 
         </nav>
     )
