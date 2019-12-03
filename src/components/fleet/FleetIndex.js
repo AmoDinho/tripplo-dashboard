19023b1b5c84386dffdfa40031337744a7e67101
AmoDinho <u14284783@tuks.co.za>
2019-12-03 16:52:07 +0200

    imported table into fleet index

--- src/components/fleet/FleetIndex.js
+++ src/components/fleet/FleetIndex.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AddAsset from './AddAsset'
 import AssetRow from './AssetRow'
+import Table from '../table/Table'
 const FleetIndex = () => {
     return (
         <>
             <AddAsset />
             <AssetRow />
+            <Table />
         </>
     )
 }
--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,6 +3,20 @@ import React from 'react'
 const Table = () => {
     return (
         <>
+            <div>
+                <div>Truks</div>
+                <diV>Issues</diV>
+            </div>
+            <table>
+                <tr>
+                    <th>Truck</th>
+                    <th>Truck details</th>
+                    <th>Driver</th>
+                    <th>Trailer(s)</th>
+                    <th>Status</th>
+                    <th colSpan="2">Issues</th>
+                </tr>
+            </table>
 
         </>
     )
