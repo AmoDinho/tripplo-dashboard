9e4025dee08f63f3b64b90eb7402227ccdcccc76
AmoDinho <u14284783@tuks.co.za>
2019-12-03 11:15:13 +0200

    created searchbar component

---
+++ src/components/inputs/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'React'
+import BodyText from ''
+const SearchBar = () => {
+    return (
+        <>
+            <div className="bg-gray-200 rounded-full p-4">
+
+            </div>
+
+        </>
+    )
+}
+
+export default SearchBar
