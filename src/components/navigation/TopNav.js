9da768d4a4fd5beb698b388885b2cf654d7241fd
AmoDinho <u14284783@tuks.co.za>
2019-12-04 10:38:22 +0200

    Everything in the navbar is alright

--- src/components/navigation/TopNav.js
+++ src/components/navigation/TopNav.js
@@ -8,23 +8,23 @@ import User from '../../assets/user.jpg'
 const TopNav = ({ isOpen, onClick, children, ...props }) => {
 
     return (
-        <nav className={'h-12 p-10 bg-gray-300 mb-5 flex items-center justify-between flex-wrap '}>
+        <nav className={'h-16 pb-10 bg-gray-300 mb-5 flex items-center justify-between flex-wrap '}>
 
-            <div className="flex items-center flex-shrink">
+            <div className="flex items-center flex-shrink ml-4">
                 <span onClick={onClick}>
                     <img src={Menu} alt="menu" />
                 </span>
             </div>
 
 
-            <div className="  lg:flex lg:items-start ">
+            <div className="  flex items-start p-2">
 
 
                 <SearchBar />
 
 
-                <img src={Grid} className="ml-8 mr-5" alt="grid" />
-                <img src={Bell} className="mr-5" alt="bell" />
+                <img src={Grid} className="ml-8 mr-5 mt-3 " alt="grid" />
+                <img src={Bell} className="mr-5  mt-3" alt="bell" />
 
                 <Avatar src={User} />
 
